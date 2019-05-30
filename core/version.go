344b2e7a9510c26f6b078b48edced483c9b9fb14
cx3ptr <bigpyer@126.com>
2019-05-30 16:33:44 +0800

    add list namespace interface, return names of all namespace

--- core/version.go
+++ core/version.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	// Version means gaea version
-	Version = "2019-05-29 15:21:02 +0800 @4d03678"
+	Version = "2019-05-30 13:36:53 +0800 @3b48f7b"
 	// Compile means gaea compole info
-	Compile = "2019-05-29 23:25:14 +0800 by go version go1.12 linux/amd64"
+	Compile = "2019-05-30 16:28:36 +0800 by go version go1.11.4 linux/amd64"
 )
