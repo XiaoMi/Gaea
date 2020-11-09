8dfd44b8c82e1c577ff579b6b11d77b3d3f1de48
maidoudou <zhangxiaoxiao6@xiaomi.com>
2020-11-09 15:20:49 +0800

    change to mockery test

--- proxy/server/executor_test.go
+++ proxy/server/executor_test.go
@@ -16,25 +16,20 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 
-	"github.com/XiaoMi/Gaea"
-
-	"github.com/stretchr/testify/assert"
-
-	"github.com/XiaoMi/Gaea/mysql"
-	"github.com/XiaoMi/Gaea/util"
-
 	"github.com/XiaoMi/Gaea/backend"
-	"github.com/golang/mock/gomock"
-
+	"github.com/XiaoMi/Gaea/backend/mocks"
+	"github.com/XiaoMi/Gaea/log"
 	"github.com/XiaoMi/Gaea/models"
-
-	"fmt"
-
+	"github.com/XiaoMi/Gaea/mysql"
 	"github.com/XiaoMi/Gaea/parser"
 	"github.com/XiaoMi/Gaea/parser/ast"
-	mockmysql "github.com/siddontang/go-mysql/mysql"
+	"github.com/XiaoMi/Gaea/util"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/ini.v1"
 )
 
 func TestGetVariableExprResult(t *testing.T) {
@@ -67,27 +62,6 @@ func TestGetVariableExprResult(t *testing.T) {
 	}
 }
 
-/*
-
-type PooledConnect interface {
-	Recycle()
-	Reconnect() error
-	Close()
-	IsClosed() bool
-	UseDB(db string) error
-	Execute(sql string) (*mysql.Result, error)
-	SetAutoCommit(v uint8) error
-	Begin() error
-	Commit() error
-	Rollback() error
-	SetCharset(charset string, collation mysql.CollationID) (bool, error)
-	FieldList(table string, wildcard string) ([]*mysql.Field, error)
-	GetAddr() string
-	SetSessionVariables(frontend *mysql.SessionVariables) (bool, error)
-	WriteSetStatement() error
-}
-*/
-
 func TestExecute(t *testing.T) {
 	se, err := prepareSessionExecutor()
 	if err != nil {
@@ -97,55 +71,56 @@ func TestExecute(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	slice0MasterPool := Gaea.NewMockConnectionPool(mockCtrl)
-	slice0SlavePool := Gaea.NewMockConnectionPool(mockCtrl)
-	slice1MasterPool := Gaea.NewMockConnectionPool(mockCtrl)
-	slice1SlavePool := Gaea.NewMockConnectionPool(mockCtrl)
+	slice0MasterPool := new(mocks.ConnectionPool)
+	slice0SlavePool := new(mocks.ConnectionPool)
+	slice1MasterPool := new(mocks.ConnectionPool)
+	slice1SlavePool := new(mocks.ConnectionPool)
 	se.manager.GetNamespace("test_executor_namespace").slices["slice-0"].Master = slice0MasterPool
 	se.manager.GetNamespace("test_executor_namespace").slices["slice-0"].Slave = []backend.ConnectionPool{slice0SlavePool}
 	se.manager.GetNamespace("test_executor_namespace").slices["slice-1"].Master = slice1MasterPool
 	se.manager.GetNamespace("test_executor_namespace").slices["slice-1"].Slave = []backend.ConnectionPool{slice1SlavePool}
 
+	expectResult1 := &mysql.Result{}
+	expectResult2 := &mysql.Result{}
+	//slice-0
 	ctx := context.Background()
-	slice0MasterConn := Gaea.NewMockPooledConnect(mockCtrl)
-	slice0MasterConn.EXPECT().Execute("INSERT into `tbl_mycat` set `k`=0").Return(nil, nil)
+	slice0MasterConn := new(mocks.PooledConnect)
+	slice0MasterPool.On("Get", ctx).Return(slice0MasterConn, nil).Once()
+	slice0MasterConn.On("UseDB", "db_mycat_0").Return(nil)
+	slice0MasterConn.On("SetCharset", "utf8", mysql.CharsetIds["utf8"]).Return(false, nil)
+	slice0MasterConn.On("SetSessionVariables", mysql.NewSessionVariables()).Return(false, nil)
+	slice0MasterConn.On("GetAddr").Return("127.0.0.1:3306")
+	slice0MasterConn.On("Execute", "SELECT * FROM `tbl_mycat` WHERE `k`=0").Return(expectResult1, nil)
+	slice0MasterConn.On("Recycle").Return(nil)
 
-	slice0slaveConn := Gaea.NewMockPooledConnect(mockCtrl)
-	//slice1slaveConn := backend.NewMockPooledConnect(mockCtrl)
-	slice0slaveConn.EXPECT().Execute("SELECT * FROM `tbl_mycat` WHERE `k`=0").Return(nil, nil).Times(2)
-	//slice1slaveConn.EXPECT().Execute("SELECT * FROM `tbl_mycat` WHERE `k`=0").Return(nil, nil).Times(2)
-
-	slice0MasterPool.EXPECT().Get(ctx).Return(slice0MasterConn, nil)
-	slice0MasterPool.EXPECT().Put(slice0MasterConn).Return()
-	slice0SlavePool.EXPECT().Get(ctx).Return(slice0slaveConn, nil)
-	slice0SlavePool.EXPECT().Put(slice0slaveConn).Return()
-
-	//slice1SlavePool.EXPECT().Get(context.TODO()).Return(slice1slaveConn, nil)
-
-	//slice1SlavePool.EXPECT().Put(slice1slaveConn).Return()
+	//slice-1
+	slice1MasterConn := new(mocks.PooledConnect)
+	slice1MasterPool.On("Get", ctx).Return(slice1MasterConn, nil).Once()
+	slice1MasterConn.On("UseDB", "db_mycat_2").Return(nil)
+	slice1MasterConn.On("SetCharset", "utf8", mysql.CharsetIds["utf8"]).Return(false, nil)
+	slice1MasterConn.On("SetSessionVariables", mysql.NewSessionVariables()).Return(false, nil)
+	slice1MasterConn.On("GetAddr").Return("127.0.0.1:3306")
+	slice1MasterConn.On("Execute", "SELECT * FROM `tbl_mycat` WHERE `k`=0").Return(expectResult2, nil)
+	slice1MasterConn.On("Recycle").Return(nil)
 
 	sqls := map[string]map[string][]string{
 		"slice-0": {
-			"db_mycat_0": {"INSERT into `tbl_mycat` set `k`=0"},
-			//"db_mycat_1": {"SELECT * FROM `tbl_mycat` WHERE `k`=0"},
+			"db_mycat_0": {"SELECT * FROM `tbl_mycat` WHERE `k`=0"},
 		},
-		//"slice-1": {
-		//	"db_mycat_2": {"SELECT * FROM `tbl_mycat` WHERE `k`=0"},
-		//	"db_mycat_3": {"SELECT * FROM `tbl_mycat` WHERE `k`=0"},
-		//},
+		"slice-1": {
+			"db_mycat_2": {"SELECT * FROM `tbl_mycat` WHERE `k`=0"},
+		},
 	}
-	expectResult := &mockmysql.Result{}
-	//ns := m.GetNamespace(namespaceName)
-	//assert.Equal()
-	//se.ExecuteCommand(mysql.ComQuery, []byte(sql))
+
+	ret := make([]*mysql.Result, 0)
+	ret = append(ret, expectResult1, expectResult2)
 
 	reqCtx := util.NewRequestContext()
 	reqCtx.Set(util.StmtType, parser.StmtInsert)
 
-	//se.isAutoCommit()
 	rs, err := se.ExecuteSQLs(reqCtx, sqls)
 	assert.Equal(t, nil, err)
-	assert.Equal(t, rs, expectResult)
+	assert.Equal(t, rs, ret)
 }
 
 func prepareSessionExecutor() (*SessionExecutor, error) {
@@ -170,7 +145,56 @@ func prepareSessionExecutor() (*SessionExecutor, error) {
 }
 
 func prepareNamespaceManager() (*Manager, error) {
-	nsStr := `
+	proxyCfg := `
+; config type, etcd/file, you can test gaea with file type, you shoud use etcd in production
+config_type=etcd
+;file config path, 具体配置放到file_config_path的namespace目录下，该下级目录为固定目录
+file_config_path=./etc/file
+;coordinator addr
+coordinator_addr=http://127.0.0.1:2379
+;远程配置(当前为etcd)根目录
+;将会废弃该配置项，通过cluster name识别root
+coordinator_root=/gaea
+;etcd user config
+username=root
+password=root
+;environ
+environ=local
+;service name
+service_name=gaea_proxy
+;gaea_proxy cluster name
+cluster_name=gaea
+;log config
+log_path=./logs
+log_level=Notice
+log_filename=gaea
+log_output=file
+
+;admin addr
+admin_addr=0.0.0.0:13307
+; basic auth
+admin_user=admin
+admin_password=admin
+
+;proxy addr
+proto_type=tcp4
+proxy_addr=0.0.0.0:13306
+proxy_charset=utf8
+
+;slow sql time, when execute time is higher than this, log it, unit: ms
+slow_sql_time=100
+
+;close session after session timeout, unit: seconds
+session_timeout=3600
+
+;stats conf
+stats_enabled=true 
+
+;encrypt key
+encrypt_key=1234abcd5678efg*
+`
+
+	nsCfg := `
 {
     "name": "test_executor_namespace",
     "online": true,
@@ -237,25 +261,40 @@ func prepareNamespaceManager() (*Manager, error) {
     "default_slice": "slice-0"
 }`
 
+	//加载proxy配置
+	var proxy = &models.Proxy{}
+	cfg, err := ini.Load([]byte(proxyCfg))
+	if err != nil {
+		return nil, err
+	}
+	if err = cfg.MapTo(proxy); err != nil {
+		return nil, err
+	}
+
+	//加载namespace配置
 	namespaceName := "test_executor_namespace"
 	namespaceConfig := &models.Namespace{}
-	if err := json.Unmarshal([]byte(nsStr), namespaceConfig); err != nil {
-		fmt.Println(err)
+	if err := json.Unmarshal([]byte(nsCfg), namespaceConfig); err != nil {
 		return nil, err
 	}
 
 	m := NewManager()
-	current, _, _ := m.switchIndex.Get()
+	// init statistics
+	statisticManager, err := CreateStatisticManager(proxy, m)
+	if err != nil {
+		log.Warn("init stats manager failed, %v", err)
+		return nil, err
+	}
+	m.statistics = statisticManager
+
 	// init namespace
+	current, _, _ := m.switchIndex.Get()
 	namespaceConfigs := map[string]*models.Namespace{namespaceName: namespaceConfig}
 	m.namespaces[current] = CreateNamespaceManager(namespaceConfigs)
-
 	user, err := CreateUserManager(namespaceConfigs)
 	if err != nil {
-		fmt.Println(err, "===============")
 		return nil, err
 	}
 	m.users[current] = user
-
 	return m, nil
 }
