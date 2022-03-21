ffef25ba932474cd39869d877c1e32afadeff041
PanHongRainbow <panhongrainbow@gmail.com>
2022-03-21 09:47:10 +0800

    test: rename func in pipeTest

--- mysql/conn_test.go
+++ mysql/conn_test.go
@@ -12,7 +12,7 @@ func TestMariadbConnWithoutDB(t *testing.T) {
 	// 函数测试开始
 	t.Run("MariaDB连接 的抽换缓存测试", func(t *testing.T) {
 		// 开始模拟
-		mockClient, mockServer := pipeTest.NewDcServerClient(t, pipeTest.TestReplyFunc) // 产生 Gaea 和 MariaDB 模拟物件
+		mockClient, mockServer := pipeTest.NewDcServerClient(t, pipeTest.TestReplyMsgFunc) // 产生 Gaea 和 MariaDB 模拟物件
 
 		// 针对这次测试进行临时修改
 		err := mockClient.OverwriteConnBufWrite(nil, writersPool.Get().(*bufio.Writer))
@@ -26,11 +26,11 @@ func TestMariadbConnWithoutDB(t *testing.T) {
 		// 开始进行讯息操作
 
 		// 写入部份
-		mockClient.SendOrReceive(msg0) // 模拟客户端传送讯息
+		mockClient.SendOrReceiveMsg(msg0) // 模拟客户端传送讯息
 		require.Equal(t, msg0[0], correct)
 
 		// 读取部份
-		msg1 := mockClient.Reply(mockServer) // 模拟服务端接收讯息
+		msg1 := mockClient.ReplyMsg(mockServer) // 模拟服务端接收讯息
 		correct++
 		require.Equal(t, msg1[0], correct)
 	})
