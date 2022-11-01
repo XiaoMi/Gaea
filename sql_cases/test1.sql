1570ec71167640889ca21fb2fd6c791cf05c99e0
yuqi1129 <yuqi4733@gmail.com>
2022-11-01 16:41:17 +0800

    add integrate test

---
+++ sql_cases/test1.sql
@@ -0,0 +1,2 @@
+select * from sbtest1.t1 where id > 1 order by id desc limit 10;
+select * from sbtest1.t2 where id < 0;
---
+++ sql_cases/test2.sql
@@ -0,0 +1,1 @@
+select * from sbtest1.t1 where id > 4 and id <=8 order by name desc;
