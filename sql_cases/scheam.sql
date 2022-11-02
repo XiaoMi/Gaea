1c292ebc0dbdba5cfc7aff7b7a8ec69e2d515b87
yuqi1129 <yuqi4733@gmail.com>
2022-11-02 17:49:01 +0800

    Add auto create database and table

---
+++ sql_cases/scheam.sql
@@ -0,0 +1,11 @@
+//Attention, You may need to name the schame sql files with 'schema' prefix and thus ensure schema files will
+//be executed first
+
+
+create database if not exists sbtest1;
+create table if not exists sbtest1.t1(id int unsigned primary key auto_increment, name varchar(20));
+insert into sbtest1.t1(name) values('x'),('y'),('z'),('zhangsan'),('lisi');
+
+create table if not exists sbtest1.t2(id int unsigned primary key auto_increment, name varchar(20));
+insert into sbtest1.t1(name) values('x'),('y'),('z'),('zhangsan'),('lisi');
+
