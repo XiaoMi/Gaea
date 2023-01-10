1fd3e3976ed4862bb5e5235b5e5bc4d13add1af6
黄锦钦 <huangjinqin@xiaomi.com>
2023-01-10 09:08:40 +0000

    fix: optimize gaea log format

--- proxy/server/executor.go
+++ proxy/server/executor.go
@@ -277,8 +277,8 @@ func (se *SessionExecutor) ExecuteCommand(cmd byte, data []byte) Response {
 	start := time.Now()
 	switch cmd {
 	case mysql.ComQuit:
-		_ = se.manager.statistics.generalLogger.Notice("Quit - namespace:[%s] conn_id=%d, %s", se.namespace,
-			se.session.c.ConnectionID, se.clientAddr)
+		_ = se.manager.statistics.generalLogger.Notice("Quit - conn_id=%d, %s, namespace:%s",
+			se.session.c.ConnectionID, se.clientAddr, se.namespace)
 		se.handleRollback(nil)
 		// https://dev.mysql.com/doc/internals/en/com-quit.html
 		// either a connection close or a OK_Packet, OK_Packet will cause client RST sometimes, but doesn't affect sql execute
@@ -288,7 +288,7 @@ func (se *SessionExecutor) ExecuteCommand(cmd byte, data []byte) Response {
 		// handle phase
 		r, err := se.handleQuery(sql)
 		if err != nil {
-			_ = se.manager.statistics.generalLogger.Notice("ERROR - %.1fms namespace:[%s] - %s->%s,mysql_connect_id=%d|%v",
+			_ = se.manager.statistics.generalLogger.Notice("ERROR - %.1fms - namespace=%s, %s->%s, mysql_connect_id=%d|%v",
 				float64(time.Since(start).Microseconds())/1000.0,
 				se.namespace,
 				se.clientAddr,
@@ -298,8 +298,8 @@ func (se *SessionExecutor) ExecuteCommand(cmd byte, data []byte) Response {
 
 			return CreateErrorResponse(se.status, err)
 		}
-		// Gaea support multi tanant, so we can set the server addr and port is a constant number
-		_ = se.manager.statistics.generalLogger.Notice("OK - %.1fms namespace:[%s] - %s->%s,mysql_connect_id=%d|%v",
+		// Gaea support multi tenant, so we can set the server addr and port is a constant number
+		_ = se.manager.statistics.generalLogger.Notice("OK - %.1fms - namespace=%s, %s->%s, mysql_connect_id=%d|%v",
 			float64(time.Since(start).Microseconds())/1000.0,
 			se.namespace,
 			se.clientAddr,
