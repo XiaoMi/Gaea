68e2fa84cb74edf009285f9e75a595873770fbe4
余国聪 <yuguocong@xiaomi.com>
2023-03-30 07:53:57 +0000

    feature: support sql `order by max()/min()`

--- sql_cases/scheam.sql
+++ sql_cases/scheam.sql
@@ -3,6 +3,8 @@
 
 
 create database if not exists sbtest1;
+drop table sbtest1.t1;
+drop table sbtest1.t2;
 create table if not exists sbtest1.t1(id int unsigned primary key auto_increment, name varchar(20));
 insert into sbtest1.t1(name) values('x'),('y'),('z'),('zhangsan'),('lisi');
 
