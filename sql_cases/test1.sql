cd87ff243b5698b9651cf75eb90b35a339f15d81
余国聪 <yuguocong@xiaomi.com>
2023-03-30 08:38:54 +0000

    Test integration promotion new

--- sql_cases/test1.sql
+++
@@ -1,5 +0,0 @@
-//Attention, You may need to name the test sql files with 'test' prefix and thus ensure test files will
-//be executed after schema file
-
-select * from sbtest1.t1 where id > 1 order by id desc limit 10;
-select * from sbtest1.t2 where id < 0;
--- sql_cases/test2.sql
+++
@@ -1,1 +0,0 @@
-select * from sbtest1.t1 where id > 4 and id <=8 order by name desc;
