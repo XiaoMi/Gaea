53095b29f13d42db06c10001bfc6e3b475355331
余国聪 <yuguocong@xiaomi.com>
2023-04-27 15:55:27 +0800

    optimize: optimize general log with user and backend addr

--- proxy/server/manager.go
+++ proxy/server/manager.go
@@ -342,7 +342,7 @@ func (m *Manager) RecordSessionSQLMetrics(reqCtx *util.RequestContext, se *Sessi
 	}
 
 	if ns.openGeneralLog {
-		m.statistics.generalLogger.Notice("client: %s, namespace: %s, db: %s, user: %s, cmd: %s, sql: %s, cost: %d ms, succ: %t",
+		m.statistics.generalLogger.Notice("client: %s, ns: %s, db: %s, user: %s, cmd: %s, sql: %s, cost: %d ms, succ: %t",
 			se.clientAddr, namespace, se.db, se.user, operation, trimmedSql, duration, err == nil)
 	}
 }
